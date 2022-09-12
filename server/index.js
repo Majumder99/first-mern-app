b9cf82fe4b7d38ca1c4576e8ecfa81b19999df2a
Sourav Majumder <96306119+Majumder99@users.noreply.github.com>
2022-09-13 00:53:51 +0600

    added login and registration

--- server/index.js
+++ server/index.js
@@ -11,9 +11,51 @@ const express = require("express");
 const dotenv = require("dotenv");
 const app = express();
 const port = process.env.PORT || 8000;
+const cors = require("cors");
+const mongoose = require("mongoose");
+const User = require("./models/userSchema");
+
+//This middleware allows us to connect the cross origin because the port is defferent in two server. Client side and server side.
+app.use(cors());
+app.use(express.json());
+
+mongoose
+  .connect("mongodb+srv://root:root@cluster0.yizcs.mongodb.net/mern-stack")
+  .then(() => {
+    console.log("connected");
+  })
+  .catch((e) => {
+    console.log(e.message);
+  });
 
 app.get("/hello", (req, res) => {
   res.send("Hello world");
+});
+
+app.post("/api/register", async (req, res) => {
+  console.log(req.body);
+  try {
+    const user = await User.create({
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password,
+    });
+    res.status(200).json(user);
+  } catch (e) {
+    res.json({ msg: e.message });
+  }
+});
+
+app.post("/api/login", async (req, res) => {
+  const result = await User.findOne({
+    email: req.body.email,
+    password: req.body.password,
+  });
+  if (result) {
+    res.status(200).json({ user: result });
+  } else {
+    res.status(400).json({ msg: "Not found" });
+  }
 });
 
 app.listen(port, () => {
